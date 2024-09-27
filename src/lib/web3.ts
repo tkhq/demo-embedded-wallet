7f60b1d791647fdb083b299cae1268be913aa1ee
Andrew Min <andrew@turnkey.io>
2024-09-27 13:09:51 -0400

    defend the faucet

--- src/lib/web3.ts
+++ src/lib/web3.ts
@@ -98,9 +98,6 @@ const getWebSocketClient = () => {
     webSocketClient = createPublicClient({
       chain: sepolia,
       transport: webSocket("wss://ethereum-sepolia-rpc.publicnode.com"),
-      // transport: webSocket(
-      //   "wss://eth-sepolia.g.alchemy.com/v2/erQ2WeonfN1VMZQM_PgCMTQB4USjPXoD"
-      // ),
     })
   }
   return webSocketClient
@@ -127,7 +124,7 @@ export const watchPendingTransactions = (
 }
 
 export const fundWallet = async (address: Address) => {
-  const amount = "0.02" // ETH
+  const amount = "0.01" // ETH
   try {
     const publicClient = getPublicClient()
     const hash = await serverFundWallet(address, parseEther(amount))
