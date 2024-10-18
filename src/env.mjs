3117be4ee91f8c5da3eda5e96fd10dce049c2d1e
Robin Arenson <robin@turnkey.io>
2024-10-18 08:34:50 -0700

    Pull Facebook API versions into env vars

--- src/env.mjs
+++ src/env.mjs
@@ -13,6 +13,7 @@ export const env = createEnv({
     NEXT_PUBLIC_ORGANIZATION_ID: z.string().min(1),
     NEXT_PUBLIC_ALCHEMY_API_KEY: z.string().min(1),
     NEXT_PUBLIC_FACEBOOK_CLIENT_ID: z.string().min(1),
+    NEXT_PUBLIC_FACEBOOK_AUTH_VERSION: z.string().min(1),
   },
   server: {
     NEXT_PUBLIC_RP_ID: z.string().optional(),
@@ -30,11 +31,16 @@ export const env = createEnv({
     TURNKEY_WARCHEST_ORGANIZATION_ID: z.string().min(1),
     WARCHEST_PRIVATE_KEY_ID: z.string().min(1),
     NEXT_PUBLIC_FACEBOOK_CLIENT_ID: z.string().min(1),
+    NEXT_PUBLIC_FACEBOOK_GRAPH_API_VERSION: z.string().min(1),
   },
   runtimeEnv: {
     NEXT_PUBLIC_APPLE_OAUTH_CLIENT_ID:
       process.env.NEXT_PUBLIC_APPLE_OAUTH_CLIENT_ID,
     NEXT_PUBLIC_FACEBOOK_CLIENT_ID: process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID,
+    NEXT_PUBLIC_FACEBOOK_GRAPH_API_VERSION:
+      process.env.NEXT_PUBLIC_FACEBOOK_GRAPH_API_VERSION,
+    NEXT_PUBLIC_FACEBOOK_AUTH_VERSION:
+      process.env.NEXT_PUBLIC_FACEBOOK_AUTH_VERSION,
     NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID:
       process.env.NEXT_PUBLIC_GOOGLE_OAUTH_CLIENT_ID,
     NEXT_PUBLIC_RP_ID: process.env.NEXT_PUBLIC_RP_ID,
