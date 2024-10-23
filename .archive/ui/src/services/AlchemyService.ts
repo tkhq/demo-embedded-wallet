e49a54b1eec6484a3cb185d27a4d305c6d0be2e6
Andrew Min <andrew@turnkey.io>
2024-10-23 17:14:17 -0400

    remove archive

--- .archive/ui/src/services/AlchemyService.ts
+++
@@ -1,8 +0,0 @@
-import { Alchemy, Network } from "alchemy-sdk";
-
-const settings = {
-  apiKey: "GgXvRB-Vj4mNQU7JC_1bPW1e39-pIFZ5",
-  network: Network.ETH_SEPOLIA,
-};
-
-export const alchemy = new Alchemy(settings);
