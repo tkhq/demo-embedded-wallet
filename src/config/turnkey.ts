6c29b8de3ad54d0b3a70844d287198f8daaa7bbf
Andrew Min <andrew@turnkey.io>
2025-06-27 14:51:56 -0400

    add configurable iframe urls

--- src/config/turnkey.ts
+++ src/config/turnkey.ts
@@ -1,24 +1,31 @@
 import { env } from "@/env.mjs"
 
-const { NEXT_PUBLIC_ORGANIZATION_ID, NEXT_PUBLIC_BASE_URL } = env
+const {
+  NEXT_PUBLIC_ORGANIZATION_ID,
+  NEXT_PUBLIC_BASE_URL,
+  NEXT_PUBLIC_AUTH_IFRAME_URL,
+  NEXT_PUBLIC_EXPORT_IFRAME_URL,
+  NEXT_PUBLIC_IMPORT_IFRAME_URL,
+} = env
 
 export const turnkeyConfig = {
   apiBaseUrl: NEXT_PUBLIC_BASE_URL,
   organizationId: NEXT_PUBLIC_ORGANIZATION_ID,
   iFrame: {
-    url: "https://auth.turnkey.com",
+    // default
+    url: NEXT_PUBLIC_AUTH_IFRAME_URL ?? "https://auth.turnkey.com",
     elementId: "turnkey-auth-iframe-element-id",
     containerId: "turnkey-auth-iframe-container-id",
     auth: {
-      url: "https://auth.turnkey.com",
+      url: NEXT_PUBLIC_AUTH_IFRAME_URL ?? "https://auth.turnkey.com",
       containerId: "turnkey-auth-iframe-container-id",
     },
     export: {
-      url: "https://export.turnkey.com",
+      url: NEXT_PUBLIC_EXPORT_IFRAME_URL ?? "https://export.turnkey.com",
       containerId: "turnkey-export-iframe-container-id",
     },
     import: {
-      url: "https://import.turnkey.com",
+      url: NEXT_PUBLIC_IMPORT_IFRAME_URL ?? "https://import.turnkey.com",
       containerId: "turnkey-import-iframe-container-id",
     },
   },
